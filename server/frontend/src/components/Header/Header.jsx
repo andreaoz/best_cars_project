f5a80d38069a8f9ee6ecb97408a1fdc21dbc57e9
Andrea Oz <andreaojedazam@gmail.com>
2025-08-01 20:42:01 -0700

    style: home page

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Header.css";
 import "../assets/style.css";
 import "../assets/bootstrap.min.css";
 
@@ -38,32 +39,28 @@ if ( curr_user !== null &&  curr_user !== "") {
 }
     return (
         <div>
-          <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
-            <div class="container-fluid">
-              <h2 style={{paddingRight: "5%"}}>Dealerships</h2>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href="/">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/about">About Us</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/contact">Contact Us</a>
-                  </li>
+          <nav class="header">
+        <div class="nav-container">
+            <a href="/" class="logo">
+                <i class="fas fa-flag-checkered"></i>
+                Best Cars Dealerships
+            </a>
+            
+            <nav>
+                <ul class="nav-menu">
+                    <li><a href="/" class="nav-link">Home</a></li>
+                    <li><a href="/about" class="nav-link">About Us</a></li>
+                    <li><a href="/contact" class="nav-link">Contact Us</a></li>
                 </ul>
-                <span class="navbar-text">
+            </nav>
+
+          <span class="navbar-text">
                   <div class="loginlink" id="loginlogout">
                   {home_page_items}
                   </div>
-                  </span>
-              </div>
-            </div>
-          </nav>
+          </span>
+        </div>
+    </nav>
         </div>
     )
 }
